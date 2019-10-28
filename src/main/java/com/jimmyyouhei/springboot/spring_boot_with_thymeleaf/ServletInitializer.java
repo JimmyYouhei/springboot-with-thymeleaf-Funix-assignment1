16ebd4f3c01be2be00cd18544ff59a8ac3e73093
Nguyen Hong Quan <nguyen.hong.quan282@gmail.com>
2019-10-28 11:56:14 +0700

    Init Release

---
+++ src/main/java/com/jimmyyouhei/springboot/spring_boot_with_thymeleaf/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.jimmyyouhei.springboot.spring_boot_with_thymeleaf;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringBootWithThymeleafApplication.class);
+	}
+
+}
---
+++ src/test/java/com/jimmyyouhei/springboot/spring_boot_with_thymeleaf/SpringBootWithThymeleafApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jimmyyouhei.springboot.spring_boot_with_thymeleaf;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootWithThymeleafApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
